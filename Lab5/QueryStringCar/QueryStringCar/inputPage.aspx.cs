d6c7d8c0a0e19d541d9464a38c37f90fa522dabe
Mukur Panchani <panchanimukur@gmail.com>
2019-08-28 18:53:00 +0530

    Add Lab5Q1

---
+++ Lab5/QueryStringCar/QueryStringCar/inputPage.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace QueryStringCar
+{
+    public partial class inputPage : System.Web.UI.Page
+    {
+        string[] manList = { "Albany", "Rooster", "Osiris", "Pegassi", "Reaper", "X80 Proto" };
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack) return;
+            foreach (string s in manList)
+                DropDownList1.Items.Add(new ListItem(s));
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string modelName = DropDownList1.SelectedItem.Text;
+            string carName = TextBox1.Text;
+            if (carName == "") return;
+            string _rurl = string.Format("showDetail.aspx?modelName={0}&carName={1}", Server.UrlEncode(modelName), Server.UrlEncode(carName));
+            Response.Redirect(_rurl);
+        }
+    }
+}
---
+++ Lab5/QueryStringCar/QueryStringCar/showDetail.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace QueryStringCar
+{
+    public partial class showDetail : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack) return;
+            main.InnerHtml = string.Format("<strong> Model >> </strong> {0} <br> <strong> Name >> </strong> {1}", Request.QueryString["modelName"], Request.QueryString["carName"]);
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("inputPage.aspx");
+        }
+    }
+}
